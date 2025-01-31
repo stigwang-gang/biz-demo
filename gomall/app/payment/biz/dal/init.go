79b77942aea0bb4a3ac228bfa0c05e759ad99d50
wangxiangang <2391108191@qq.com>
2025-01-31 16:16:04 -0500

    payment module finished

---
+++ gomall/app/payment/biz/dal/init.go
@@ -0,0 +1,11 @@
+package dal
+
+import (
+	"github.com/stigwang-gang/biz-demo/gomall/app/payment/biz/dal/mysql"
+	"github.com/stigwang-gang/biz-demo/gomall/app/payment/biz/dal/redis"
+)
+
+func Init() {
+	redis.Init()
+	mysql.Init()
+}
